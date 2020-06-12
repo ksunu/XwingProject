19d199e0384cdc5b85b2bb9d3e21d3fddfb308e8
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-06-12 16:06:28 +0200

    Co-authored-by: ksunu <soonk@outlook.com>

---
+++ JS/game.js
@@ -0,0 +1,21 @@
+const Game = {
+    title: 'X Wing Project',
+    author: 'Soon Woo Kwon, Roberto Gonzalez Camejo',
+    licence: null,
+    version: '1.0',
+    canvasDom: undefined,
+    ctx: undefined,
+    canvasSize: {
+        w: undefined,
+        h: undefined
+    },
+    
+    keys: {
+        LEFT: 37,
+        RIGTH: 39,
+        // UP: 38,
+        // DOWN: 40,
+        SPACE: 32,
+        // ENTER: 13,
+    }
+}
